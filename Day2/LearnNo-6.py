f29b60e3aed7c9ae3008becf74bca4325755c6ba
PlayTisT <145091800+PlayTisT@users.noreply.github.com>
2024-04-28 19:45:26 +0700

    [Update] Done to Learn

---
+++ Day2/LearnNo-6.py
@@ -0,0 +1,23 @@
+#ติดตั้งลองใช้งาน python ด้วย VScode
+#หัวข้อหัด 6 #Operators and input
+
+name = input() 
+print("Hello", name)   #value จะใช้ karn หลัง Run
+
+#ลองกด Run จะได้ใส่ข้อความแทนค่า name  karn 
+#เป็น Hello karn
+
+
+
+#=================================
+d = input()
+x = int(d)  #จำนวนเต็มแน่ๆใช้เป็น int,ถ้าจำนวนจริงใช้ float 
+perimeter = x + x + x + x
+print("perimeter of square =", perimeter)
+#output เป็น ต้องใส่ input ลองใส่ 10
+#  perimeter of square = 40
+
+#หรือลดรูป  เป็น
+x = float(input())
+perimeter = x + x + x + x
+print("perimeter of square =", perimeter)
