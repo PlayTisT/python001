87f5df112d0536b00370165158cc5ef3898ddf2a
PlayTisT <145091800+PlayTisT@users.noreply.github.com>
2024-04-28 19:35:03 +0700

    [update] done to Learn

--- Day1/LearnNo-1.py
+++ Day1/LearnNo-1.py
@@ -1,1 +1,33 @@
-print("hello world")
+#ติดตั้งลองใช้งาน python ด้วย VScode
+#หัวข้อหัด1  String, Number and print()
+## begining
+#หัวข้อหัด1  Number and print()
+#integer:
+x = 10
+
+#float:
+x = 10.234
+
+#complex: 
+x = 25j
+
+#Booleans:
+#ออกเป็น True or False 
+
+print(1.963*10)  # output =  19.630000000000003
+
+#====================================================
+#หัวข้อหัด1  String na
+
+print ("Hello world Basic Python") # output = คำว่า  Hello world Basic Python
+
+#ตัวอย่างต่อ
+x = 'hello'
+y = "world"
+z = input()
+
+print ("here is an example, x, y, z")
+
+#end
+
+
