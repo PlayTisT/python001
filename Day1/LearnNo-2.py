9c161deab434884af8a3a239880132920130c2a3
PlayTisT <145091800+PlayTisT@users.noreply.github.com>
2024-04-28 19:37:34 +0700

    [Create] done to learn

---
+++ Day1/LearnNo-2.py
@@ -0,0 +1,16 @@
+#ติดตั้งลองใช้งาน python ด้วย VScode
+#หัวข้อหัด2  Basic Variables
+
+
+x = 222     # ให้ตัวแปร x มีค่า 222
+y = 333     # ให้ตัวแปร y มีค่า 333
+z = x + y   # output ต้องออกไป หา z
+
+print (z)   # Output z ที่คำนวณมาให้  เท่ากับ  444
+
+
+#หัวข้อหัด2 Basic Variables (ต่อ)
+
+x = 233
+x = x + 999
+print(x)  #Output ได้  1232
