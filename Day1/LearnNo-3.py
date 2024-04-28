04192048176889cb7d8ce0520895c7dd24a60970
PlayTisT <145091800+PlayTisT@users.noreply.github.com>
2024-04-28 19:39:57 +0700

    [Create] done to learn

---
+++ Day1/LearnNo-3.py
@@ -0,0 +1,17 @@
+#ติดตั้งลองใช้งาน python ด้วย VScode
+#หัวข้อหัด 3 #Basic Data Type
+
+# กำหนด  a 
+a = 50      #จำนวนเต็มมีค่า 50
+b = 15.0    #จำนวนเต็มมีค่า 15
+t = "ABC"   #สตริง ABC
+q = True    #ค่าจริง True or false
+a = [2,3,4] #เก็บรายการ 2, 3, 4
+
+#ลองเขียนออกแสดงผลด้วย Print
+print(type (a))
+print(type (b))
+print(type (t))
+print(type (q))
+print(type (a))
+
---
+++ Day1/LearnNo-4.py
@@ -0,0 +1,6 @@
+#ติดตั้งลองใช้งาน python ด้วย VScode
+#หัวข้อหัด 4 #Basic Variables & Data Type
+
+#List#  
+Fruits = ['apple', 'kiwi', 'banana']
+print(Fruits)
