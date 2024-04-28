bacc66c3eebb1f31de5fc9120c7bd208b8fe0184
PlayTisT <145091800+PlayTisT@users.noreply.github.com>
2024-04-28 19:41:31 +0700

    [Created] Learn No 5

---
+++ Day1/LearnNo-5.py
@@ -0,0 +1,10 @@
+#ติดตั้งลองใช้งาน python ด้วย VScode
+#หัวข้อหัด 5 #Type Conversions
+
+#ให้ S1 = '123'  
+#ให้ S2 = '456'
+
+n= int(S1) + int(S2)    # output ได้ 579
+m= float(S1) + float(S2) # output ได้ 579.0
+
+print(S1+S2) # ได้  123456
