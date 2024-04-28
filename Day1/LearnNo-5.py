d3bcf5521d26db99e90b845c571a607b0a9d13bb
PlayTisT <145091800+PlayTisT@users.noreply.github.com>
2024-04-28 19:42:18 +0700

    [Update] No 5

--- Day1/LearnNo-5.py
+++ Day1/LearnNo-5.py
@@ -8,3 +8,11 @@ n= int(S1) + int(S2)    # output ได้ 579
 m= float(S1) + float(S2) # output ได้ 579.0
 
 print(S1+S2) # ได้  123456
+
+#=========================
+
+print(S1+S2, n, m)
+print(S1+S2 + ", " + str(n) + ", " + str(m))
+
+# output  123456    579 579.0  
+# output  123456,   579 579.0
